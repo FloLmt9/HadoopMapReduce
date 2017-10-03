03d4095400c9b5863fefaf631c9c9868922e337b
FloLmt9 <florian.limermont@orange.fr>
2017-10-03 23:34:19 +0200

    PivotTable_v2

--- PivotTranslation.java
+++ PivotTranslation.java
@@ -3,11 +3,15 @@ package mapreduce;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.StringTokenizer;
-import javax.naming.Context;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class PivotTranslation {
 
@@ -42,4 +46,21 @@ public class PivotTranslation {
         }
         context.write(key, txt);
     }
-  }}
+  }
+  
+  
+public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "PivotTable");
+    job.setJarByClass(PivotTranslation.class);
+    job.setMapperClass(Map.class);
+    job.setCombinerClass(Reduce.class);
+    job.setReducerClass(Reduce.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+
+}
